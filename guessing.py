5abfdd72554280d933258df76e0445ac0bd1d0ba
teq1901 <rhea.anderson01@gmail.com>
2022-07-26 17:09:05 -0500

    added input

--- guessing.py
+++ guessing.py
@@ -1,5 +1,9 @@
 import random
 
 #generate a random number
-num = random.randint(1, 10)
-print(num)
+num = random.randint(1, 3)
+guess= int(input("Guess the number: "))
+if guess == num:
+    print("Yay!! you've guessed the right number!")
+else:
+    print(f"Sorry wrong guess. The correct number was {num}")
