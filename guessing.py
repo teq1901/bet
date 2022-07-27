b4adbfb734aaa2d6135989f7dc9590b22622cdbd
teq1901 <rhea.anderson01@gmail.com>
2022-07-27 16:57:10 -0500

    added while loop to repeat game

--- guessing.py
+++ guessing.py
@@ -1,9 +1,14 @@
 import random
 
-#generate a random number
-num = random.randint(1, 3)
-guess= int(input("Guess the number: "))
-if guess == num:
-    print("Yay!! you've guessed the right number!")
-else:
-    print(f"Sorry wrong guess. The correct number was {num}")
+repeat = "y"
+
+while repeat == "y":
+    #generate a random number
+    num = random.randint(1, 3)
+    guess= int(input("Guess a number between 1 and 3: "))
+    if guess == num:
+        print("Yay!! you've guessed the right number!")
+    else:
+        print(f"Sorry wrong guess. The correct number was {num}")
+
+    repeat = input("Do you want to try again? y/n: ")
